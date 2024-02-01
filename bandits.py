20fc8852b39618810fd49db37872febdeebea7a1
sudheendra07 <sudheendra.r9070@gmail.com>
2024-02-01 15:39:06 +0530

    added ucb bandits

--- bandits.py
+++ bandits.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from operator import add
 # import random
 
 def play_random(arms):
@@ -16,12 +17,36 @@ def play_greedy(arms):
     arms.mean_est[idx] += (1/arms.count_pulls[idx]) * (reward - arms.mean_est[idx])
     return reward
 
+def play_ucb(arms, t):
+    c = 2
+    if t < arms.no_arms:
+        idx = t
+    else:
+        conf_int = [(c * np.sqrt(np.log(t)/n_t)) for n_t in arms.count_pulls]
+        arms.ucb_est = list(map(add, arms.mean_est, conf_int))
+        idx = np.argmax(arms.ucb_est)
+    
+    reward = np.random.uniform(arms.mean[idx]-arms.half[idx], arms.mean[idx]+arms.half[idx])
+    try:
+        arms.count_pulls[idx] += 1
+        arms.mean_est[idx] += (1/arms.count_pulls[idx]) * (reward - arms.mean_est[idx])
+    except:
+        print(idx)
+    
+    return reward
+
 class arms_unif:
     def __init__(self, mean, half):
         self.mean = mean
         self.half = half
-        self.mean_est = [0]*len(mean)
-        self.count_pulls = [0]*len(mean)
+        self.no_arms = len(mean)
+        self.mean_est = [0]*self.no_arms
+        self.count_pulls = [0]*self.no_arms
+        self.ucb_est = [0]*self.no_arms
+
+    def reset(self):
+        self.count_pulls = [0]*self.no_arms
+        self.mean_est = [0]*self.no_arms
 
 arms_mean = [-0.3, 0, 0.2, 0.3]
 arms_unif_half = [1, 1, 1, 1]
@@ -30,13 +55,9 @@ epsilon = 0.1
 
 horizon_length = 1000
 avg_reward = np.zeros(horizon_length+1)
+avg_reward_ucb = np.zeros(horizon_length+1)
 
 arms = arms_unif(mean=arms_mean, half=arms_unif_half)
-
-mean_est = [0]*num_arms
-count_pulls = [0]*num_arms
-
-
 
 for i in range(horizon_length):
     chance = np.random.uniform(0, 1)
@@ -47,8 +68,15 @@ for i in range(horizon_length):
 
     avg_reward[i+1] = avg_reward[i] + (1/(i+1)) * (reward - avg_reward[i])
 
-plt.plot(avg_reward)
-plt.title('Epsilon-Greedy Bandit Algorithm')
+arms.reset()
+
+for i in range(horizon_length):
+    reward = play_ucb(arms, i+1)
+    avg_reward_ucb[i+1] = avg_reward_ucb[i] + (1/(i+1)) * (reward - avg_reward_ucb[i])
+
+plt.plot(avg_reward, 'g', label = f'E-G (eps = {epsilon})')
+plt.plot(avg_reward_ucb, 'r', label = f'UCB (c = 2)')
+plt.title('Epsilon-Greedy v/s UCB Bandit Algorithm')
 plt.xlabel('Steps')
 plt.ylabel('Average Cumulative Reward')
 plt.legend()
